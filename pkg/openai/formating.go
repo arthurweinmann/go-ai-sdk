2b908cb7f84506e5e3f8f524d288a0b004d4a83a
Arthur Weinmann <arthur.weinmann@gmail.com>
2023-06-09 21:04:08 -0500

    fix format prompt helper

--- pkg/openai/formating.go
+++ pkg/openai/formating.go
@@ -6,7 +6,7 @@ import (
 )
 
 var regexReplaceConsecutiveNewLine = regexp.MustCompile(`\n{2,}`)
-var regexReplaceConsecutiveSpaces = regexp.MustCompile(`\s{2,}`)
+var regexReplaceConsecutiveSpaces = regexp.MustCompile(` {2,}`)
 
 func FormatPrompt(p string) string {
 	return regexReplaceConsecutiveSpaces.ReplaceAllString(regexReplaceConsecutiveNewLine.ReplaceAllString(strings.ReplaceAll(strings.ReplaceAll(p, "\r", "\n"), "\t", "\n"), "\n"), " ")
